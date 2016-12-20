309b81f9cecccc174862f656d5cdb274508b2c01
tetondan <dfrehner1@gmail.com>
2016-12-19 22:01:07 -0700

    added get route

--- server/dbconfig.js
+++ server/dbconfig.js
@@ -7,7 +7,7 @@ const config = {
   host: 'localhost', // Server hosting the postgres database
   port: 5432, //env var: PGPORT
   max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  idleTimeoutMillis: 3000, // how long a client is allowed to remain idle before being closed
 };
 
 const pool = new pg.Pool(config);
--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.post('/create/user', ( req, res ) => {
   pg.query('INSERT INTO users (user_name, email, password) VALUES ($1, $2, $3) RETURNING id;', [body.username, body.email, body.password], (err, results) => {
     if(err){
       res.status(500).send("Error creating user");
-      return console.error('error creating user', err);
+      return console.error('Error creating user', err);
     } else {
       res.status(201).send(results.rows[0]);
     }
@@ -41,7 +41,7 @@ app.post('/create/survey', (req, res) => {
   pg.connect( (err, client, done) => {
     if(err) {
       done();
-      return console.error('error fetching client from pool', err);
+      return console.error('Error fetching client from pool', err);
     } 
 
     client.query('INSERT INTO polls (title, created_at, created_by, url, private) VALUES ($1, $2, $3, $4, $5) RETURNING id;', 
@@ -49,9 +49,10 @@ app.post('/create/survey', (req, res) => {
       if(err) {
         done();
         res.status(500).send("Error");
-        return console.error('error running query 1', err);
+        return console.error('Error creating poll', err);
       } else {
         let pollId = results.rows[0].id;
+        //TODO check to see if options are not empty
         let optionsQuery = pollQueryBuilder(pollId, body.options.length);
         client.query(optionsQuery, body.options, ( err, results ) => {
           done();
@@ -81,7 +82,7 @@ app.post('/create/comment', ( req, res ) => {
 
 app.post('/vote', ( req, res ) => {
   const body = req.body;
-  pg.query('INSERT INTO votes ( option_id, user_id, comments_id, polls_id, type) VALUES ($1, $2, $3, $4);', 
+  pg.query('INSERT INTO votes ( option_id, user_id, comments_id, polls_id) VALUES ($1, $2, $3, $4);', 
             [(body.optionid || null), (body.userid || 0), (body.commentid || null), (body.pollid || null)], ( err, results ) => {
     if(err){
       res.status(500).send("Error creating vote");
@@ -89,6 +90,54 @@ app.post('/vote', ( req, res ) => {
     } else {
       res.status(201).send(results.rows);
     }
+  })
+})
+
+app.get('/polls/:title', ( req, res ) => {
+  const title = req.params.title;
+  let pollData;
+  pg.connect( ( err, client, done ) => {
+    if(err) {
+      done();
+      res.status(500).send("Error");
+      return console.error('Error fetching client from pool', err);
+    } 
+    client.query(`SELECT polls.id as poll_id, polls.title, polls.url, polls.private, polls.created_at, 
+                    polls.created_by, COUNT(votes.polls_id) as votes
+                  FROM polls 
+                  INNER JOIN votes 
+                  ON polls.id = votes.polls_id 
+                  WHERE polls.url = $1
+                  GROUP BY polls.id`, [title],(err, results) => {
+        if(err){
+        done();
+        res.status(500).send("Error retrieving poll");
+        return console.error('error retrieving poll', err);
+      } else if( results.rows.length < 1 ){
+        done();
+        res.status(404).send("<html><body><h1>404</h1><p>Error locating poll</p></body></html>")
+      } else {
+        pollData = results.rows[0];
+        client.query(`SELECT options.option, COUNT(votes.option_id) 
+                        FROM options 
+                        LEFT OUTER JOIN votes 
+                        ON options.id = votes.option_id 
+                        WHERE options.poll_id = $1 
+                        GROUP BY options.id;`, 
+          [pollData.poll_id], ( err, results ) => {
+            if(err){
+              done();
+              res.status(500).send("Error retrieving options");
+              return console.error("Error retreiving options", err);
+            } else {
+              done();
+              pollData.options = results.rows;
+              res.status(200).send(pollData);
+            }
+          }
+        )
+      }
+    })
   })
 })
 
